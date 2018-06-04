5ccd438ad42c98542ccdd7ce0e97a35bc13fb827
Moon Antonio <pinchodev91@gmail.com>
2018-06-04 22:28:33 +0200

    Agregado data para las escenas

--- Algoritmos Project/Assets/Algoritmos/Scripts/ManagerAlgoritmos.cs
+++ Algoritmos Project/Assets/Algoritmos/Scripts/ManagerAlgoritmos.cs
@@ -27,6 +27,19 @@ namespace MoonAntonio
 		public List<Transform> escenas = new List<Transform>();
 		#endregion
 
+		#region Variables Publicas UI
+		public Text headerOrdenacion;
+		public Text descripcionOrdenacion;
+		public Slider efectividadOrdenacion;
+		public Slider velocidadOrdenacion;
+		public Toggle randomOrdenacion;
+		public Text headerBusqueda;
+		public Text descripcionBusqueda;
+		public Slider efectividadBusqueda;
+		public Slider velocidadBusqueda;
+		public Toggle randomBusqueda;
+		#endregion
+
 		#region Variables Protegidas
 		/// <summary>
 		/// <para>Id del algoritmo.</para>
@@ -99,7 +112,64 @@ namespace MoonAntonio
 		/// <param name="id"></param>
 		private void InitEscena(int id)
 		{
+			switch (id)
+			{
+				case 0:
+					headerOrdenacion.text = "Inserción";
+					descripcionOrdenacion.text = "Es una manera muy natural de ordenar para un ser humano, y puede usarse fácilmente para ordenar un mazo de cartas numeradas en forma arbitraria. Requiere O(n²) operaciones para ordenar una lista de n elementos.";
+					efectividadOrdenacion.value = 0.2f;
+					velocidadBusqueda.value = 0.2f;
+					randomOrdenacion.isOn = false;
+					break;
 
+				case 1:
+					headerOrdenacion.text = "Burbuja";
+					descripcionOrdenacion.text = "Funciona revisando cada elemento de la lista que va a ser ordenada con el siguiente, intercambiándolos de posición si están en el orden equivocado.";
+					efectividadOrdenacion.value = 0.7f;
+					velocidadBusqueda.value = 0.6f;
+					randomOrdenacion.isOn = false;
+					break;
+
+				case 2:
+					headerOrdenacion.text = "Selección";
+					descripcionOrdenacion.text = "Este algoritmo realiza muchas menos operaciones intercambiar() que el de la burbuja, por lo que lo mejora en algo.";
+					efectividadOrdenacion.value = 0.9f;
+					velocidadBusqueda.value = 0.9f;
+					randomOrdenacion.isOn = false;
+					break;
+
+				case 3:
+					headerOrdenacion.text = "Montones";
+					descripcionOrdenacion.text = "Este algoritmo consiste en almacenar todos los elementos del vector a ordenar en un montículo (heap), y luego extraer el nodo que queda como nodo raíz del montículo (cima) en sucesivas iteraciones obteniendo el conjunto ordenado.";
+					efectividadOrdenacion.value = 0.8f;
+					velocidadBusqueda.value = 0.7f;
+					randomOrdenacion.isOn = false;
+					break;
+
+				case 4:
+					headerBusqueda.text = "A*";
+					descripcionBusqueda.text = "A* encuentra, siempre y cuando se cumplan unas determinadas condiciones, el camino de menor coste entre un nodo origen y uno objetivo.";
+					efectividadBusqueda.value = 0.5f;
+					velocidadBusqueda.value = 0.9f;
+					randomBusqueda.isOn = false;
+					break;
+
+				case 5:
+					headerBusqueda.text = "Dijkstra";
+					descripcionBusqueda.text = "La idea subyacente en este algoritmo consiste en ir explorando todos los caminos más cortos que parten del vértice origen y que llevan a todos los demás vértices.";
+					efectividadBusqueda.value = 0.8f;
+					velocidadBusqueda.value = 0.5f;
+					randomBusqueda.isOn = false;
+					break;
+
+				default:
+					headerOrdenacion.text = "Inserción";
+					descripcionOrdenacion.text = "Es una manera muy natural de ordenar para un ser humano, y puede usarse fácilmente para ordenar un mazo de cartas numeradas en forma arbitraria. Requiere O(n²) operaciones para ordenar una lista de n elementos.";
+					efectividadOrdenacion.value = 0.2f;
+					velocidadBusqueda.value = 0.2f;
+					randomOrdenacion.isOn = false;
+					break;
+			}
 		}
 		#endregion
 	}
